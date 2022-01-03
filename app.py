9b294840a03bd621c55f8a75c5cce28ec814b490
Deepesh Bhatia <db@gmail.com>
2022-01-03 22:07:17 +0530

    added circle ci

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def message():
 
-    return render_template('home.html')
+    return render_template('message.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
